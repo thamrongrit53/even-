7154255681544a9f765234b90cac8ed222a4dd16
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-08-18 11:41:18 +0700

    behavior

--- std_report_behavior.php
+++ std_report_behavior.php
@@ -2,15 +2,22 @@
 require_once('condb.php');
 require_once('session_std.php');
 require_once('navbar.php'); 
-
+$std_id=$_SESSION["std_id"];
+$query1="SELECT SUM(score) AS num1 FROM behavior WHERE std_id='$std_id'";
+$result1 = mysqli_query($condb,$query1);
+$objResult1 = mysqli_fetch_array($result1,MYSQLI_ASSOC);
 ?>
 
 <div class="container" style="margin-top: 20px;">
 	<div class="row">
 		<div class="col-md-12">
 			<div class="text-center">
-				<h3>รายงานคะแนนพฤติกรรม</h3>
-
+       <div class="card bg-dark text-white" style="border-radius: 20px;">
+    <div class="card-body text-center">
+   <h2>รายงานคะแนนพฤติกรรม</h2><br>
+       <h2>เหลือ<?php echo 100-$objResult1["num1"]; ?> &nbsp;คะแนน</h2><br> 
+      </div>
+      </div>
     		
     		<div id="result"></div>
     	</div>
